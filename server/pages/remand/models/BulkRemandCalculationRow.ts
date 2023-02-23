eceeae20cb45117fda05d8e3eceb341c2651f1a9
ldlharper <luke.harper@digital.justice.gov.uk>
2023-02-23 11:08:42 +0000

    MDWFP-66 fixing remand not displayed. (#20)

--- server/pages/remand/models/BulkRemandCalculationRow.ts
+++ server/pages/remand/models/BulkRemandCalculationRow.ts
@@ -5,8 +5,10 @@ type BulkRemandCalculationRow = {
   COURT_DATES_JSON: string
   CALCULATED_ALL_JSON: string
   NOMIS_REMAND_DAYS: number
+  NOMIS_UNUSED_REMAND_DAYS: number
   CALCULATED_REMAND_DAYS: number
   NOMIS_REMAND_JSON: string
+  NOMIS_UNUSED_REMAND_JSON: string
   CALCULATED_REMAND_JSON: string
   IS_REMAND_SAME: 'Y' | 'N'
   IS_DAYS_SAME: 'Y' | 'N'
