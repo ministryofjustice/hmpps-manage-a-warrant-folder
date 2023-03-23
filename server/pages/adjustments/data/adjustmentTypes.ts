3e7a42a1db8220f39b60bba61689d5ac453cc410
Luke Harper <luke.harper@digital.justice.gov.uk>
2023-03-23 12:54:31 +0000

    MDWFP-98 content updates

--- server/pages/adjustments/data/adjustmentTypes.ts
+++ server/pages/adjustments/data/adjustmentTypes.ts
@@ -1,6 +1,7 @@
 export type AdjustmentType = {
   value: string
   text: string
+  shortText: string
   url: string
 }
 
@@ -8,41 +9,49 @@ const adjustmentTypes: AdjustmentType[] = [
   {
     value: 'REMAND',
     text: 'Remand',
+    shortText: 'remand',
     url: 'remand',
   } as AdjustmentType,
   {
     value: 'TAGGED_BAIL',
     text: 'Tagged bail',
+    shortText: 'tagged bail',
     url: 'tagged-bail',
   } as AdjustmentType,
   {
     value: 'LAWFULLY_AT_LARGE',
     text: 'Lawfully at large',
+    shortText: 'lawfully at large',
     url: 'lawfully-at-large',
   } as AdjustmentType,
   {
     value: 'UNLAWFULLY_AT_LARGE',
-    text: 'Unlawfully at large',
+    text: 'Unlawfully at large (UAL)',
+    shortText: 'UAL',
     url: 'unlawfully-at-large',
   } as AdjustmentType,
   {
     value: 'RESTORED_ADDITIONAL_DAYS_AWARDED',
-    text: 'Restored additional days awarded',
+    text: 'Restore additional days awarded (RADA)',
+    shortText: 'RADA',
     url: 'restored-additional-days',
   } as AdjustmentType,
   {
     value: 'ADDITIONAL_DAYS_AWARDED',
-    text: 'Additional days awarded',
+    text: 'Additional days awarded (ADA)',
+    shortText: 'ADA',
     url: 'additional-days',
   } as AdjustmentType,
   {
     value: 'SPECIAL_REMISSION',
     text: 'Special remission',
+    shortText: 'special remission',
     url: 'special-remission',
   } as AdjustmentType,
   {
     value: 'TIME_SPENT_IN_CUSTODY_ABROAD',
     text: 'Time spent in custody abroad',
+    shortText: 'time spent in custody',
     url: 'spent-abroad',
   } as AdjustmentType,
 ]
